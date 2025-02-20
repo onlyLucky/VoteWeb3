fe6b656c91209afe7bc803b5f70d6e55a8362c48
Feynman <2901954062@qq.com>
2025-02-20 17:42:00 +0800

    workflow: 新增后台管理系统基础框架结构。

---
+++ admin/src/layouts/components/Hamburger/index.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import { useDevice } from "@@/composables/useDevice"
+import { Expand, Fold } from "@element-plus/icons-vue"
+
+const props = withDefaults(defineProps<Props>(), {
+  isActive: false
+})
+
+const emit = defineEmits<{
+  toggleClick: []
+}>()
+
+const { isMobile } = useDevice()
+
+interface Props {
+  isActive?: boolean
+}
+
+function toggleClick() {
+  emit("toggleClick")
+}
+</script>
+
+<template>
+  <div @click="toggleClick" v-show="isMobile">
+    <el-icon :size="20" class="icon">
+      <Fold v-if="props.isActive" />
+      <Expand v-else />
+    </el-icon>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.icon {
+  vertical-align: middle;
+  color: var(--v3-hamburger-text-color);
+}
+</style>
